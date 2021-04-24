cf64a9d141dfd075a006cfa5d44abe1b1190e046
Avazjon Yusufjonov <azizaziz9911@mail.ru>
2021-04-24 17:30:36 +0900

    Finish 1.6-1.7

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,66 +1,90 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>{props.courseName} {props.numOfExercises}</p>
-    </div>
-  )
-}
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
 
-const Header = (props) => {
+const Statistics = ({good, neutral, bad, all, average, positivePercentage}) => {
   return (
-    <div>
-      <p>{props.courseName}</p>
-    </div>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part courseName={props.courseNames[0]} numOfExercises={props.numOfExercises[0]} />
-      <Part courseName={props.courseNames[1]} numOfExercises={props.numOfExercises[1]} />
-      <Part courseName={props.courseNames[2]} numOfExercises={props.numOfExercises[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>{props.total}</p>
-    </div>
+  <div>
+    <p>Good {good}</p>
+    <p>Neutral {neutral}</p>
+    <p>Bad {bad}</p>
+    <p>All {all}</p>
+    <p>Average {average}</p>
+    <p>Positive {positivePercentage}</p>
+  </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
+  const [feedback, setFeedback] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
+  const [allFeedbacks, setAll] = useState([])
+
+
+  const incrementGoodFeedback = () => {
+        const newFeedback = {
+        ...feedback,
+        good: feedback.good + 1
       }
-    ]
+      setFeedback(newFeedback)
+      setAll(allFeedbacks.concat(1))
+      console.log(allFeedbacks)
+    }
+  const incrementNeutralFeedback = () => {
+    const newFeedback = {
+      ...feedback,
+      neutral: feedback.neutral + 1
+    }
+    setFeedback(newFeedback)
+    setAll(allFeedbacks.concat(0))
+  }
+  const incrementBadFeeback = () => {
+    const newFeedback = {
+      ...feedback,
+      bad: feedback.bad + 1
+    }
+    setFeedback(newFeedback)
+    setAll(allFeedbacks.concat(-1))
+  }
+
+  const average = (arr) => {
+    const reducer = (accumulator, currentVal) => accumulator + currentVal
+    if (arr.length > 1) {
+     const avg = arr.reduce(reducer) / arr.length;
+     return avg;
+    }
+    return 0;
+  }
+  
+  const positivePercentage = (positiveFeedbacks, all) => {
+    if (all.length > 1 && positiveFeedbacks > 1) {
+      return positiveFeedbacks / all.length;
+    }
+    return 0;
   }
 
   return (
     <div>
-      <Header courseName={course.name} />
-      <Content courseNames={course.parts.map(part => part.name)} numOfExercises={course.parts.map(part => part.exercises)} />
-      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
+      <h1>Give feedbaallFeedbacks
+allFeedbacksck</h1>
+      <Button handleClick={incrementGoodFeedback} text='Good' />
+      <Button handleClick={incrementNeutralFeedback} text='Neutral' />
+      <Button handleClick={incrementBadFeeback} text='Bad' />
+      <h1>Statistics</h1>
+      <Statistics 
+        good={feedback.good} 
+        neutral={feedback.neutral} 
+        bad={feedback.bad} 
+        all={allFeedbacks.length + 5}
+        average={average(allFeedbacks)} 
+        positivePercentage={positivePercentage(feedback.good, allFeedbacks)}
+      />   
     </div>
   )
+
 }
+
 
 
 export default App;
--- part1/src/index.js
+++ part1/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+  <App />,
+document.getElementById('root'))
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
