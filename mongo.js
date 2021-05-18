4cabbdbadac765b955a6798218a2f6f88c4ee166
Avazjon Yusufjonov <azizaziz9911@mail.ru>
2021-05-18 15:22:52 +0900

    Finish 3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://user8889:${password}@contacts-app.5wdr9.mongodb.net/contact-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  id: Number,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+if (process.argv.length > 3) {
+  const note = new Note({
+    name: process.argv[3],
+    number: process.argv[4]
+  })
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  console.log(process.argv)
+
+} else if (process.argv.length === 3) {
+  Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note)
+    })
+    mongoose.connection.close()
+})
+}
