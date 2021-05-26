46baab190d1956f43074d8626f38607c13e69534
Avazjon Yusufjonov <azizaziz9911@mail.ru>
2021-05-26 20:32:58 +0900

    Check

--- models/contact.cjs
+++ models/contact.cjs
@@ -1,4 +1,5 @@
-require('dotenv').config({path: '../.env'})
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const mongoose = require('mongoose')
 const uri = process.env.MONGO_URI
 console.log(typeof uri)
